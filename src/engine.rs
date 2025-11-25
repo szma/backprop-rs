554ee9d6465c0b5b4647d20dba66f520be7ac890
Markus Schütz <schuetz@mailbox.org>
2025-11-25 22:18:00 +0100

    clippy

--- src/engine.rs
+++ src/engine.rs
@@ -8,6 +8,12 @@ pub struct Context {
     vars: Vec<Variable>,
 }
 
+impl Default for Context {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Context {
     pub fn new() -> Self {
         Self { vars: Vec::new() }
