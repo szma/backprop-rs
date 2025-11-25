c0487a9c688c00e1b40163f38484a07473b501a4
Markus Schütz <schuetz@mailbox.org>
2025-11-25 20:48:00 +0100

    nicer variable init

--- src/main.rs
+++ src/main.rs
@@ -7,81 +7,59 @@ struct Context {
 }
 
 impl Context {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self { vars: Vec::new() }
     }
 
-    fn var(&mut self, data: f64) -> VariableIdx {
+    pub fn var(&mut self, data: f64) -> VariableIdx {
         let new_var = Variable::new(data);
         self.vars.push(new_var);
         self.vars.len() - 1
     }
 
-    fn add(&mut self, a: VariableIdx, b: VariableIdx) -> VariableIdx {
-        let va = &self.vars[a];
-        let vb = &self.vars[b];
-        let new_var = Variable {
-            data: va.data + vb.data,
+    // Helper to construct and return new variable
+    fn push_var(&mut self, data: f64, children: Vec<VariableIdx>, op: Op) -> VariableIdx {
+        self.vars.push(Variable {
+            data,
             grad: None,
-            children: vec![a, b],
-            op: Op::Add,
-        };
-
-        self.vars.push(new_var);
+            children,
+            op,
+        });
         self.vars.len() - 1
     }
 
-    fn mul(&mut self, a: VariableIdx, b: VariableIdx) -> VariableIdx {
-        let va = &self.vars[a];
-        let vb = &self.vars[b];
-        let new_var = Variable {
-            data: va.data * vb.data,
-            grad: None,
-            children: vec![a, b],
-            op: Op::Mul,
-        };
-
-        self.vars.push(new_var);
-        self.vars.len() - 1
+    pub fn add(&mut self, a: VariableIdx, b: VariableIdx) -> VariableIdx {
+        let data = self.vars[a].data + self.vars[b].data;
+        self.push_var(data, vec![a, b], Op::Add)
     }
 
-    fn pow(&mut self, a: VariableIdx, exp: f64) -> VariableIdx {
-        let va = &self.vars[a];
-        let new_var = Variable {
-            data: va.data.powf(exp),
-            grad: None,
-            children: vec![a],
-            op: Op::Pow(exp),
-        };
-
-        self.vars.push(new_var);
-        self.vars.len() - 1
+    pub fn mul(&mut self, a: VariableIdx, b: VariableIdx) -> VariableIdx {
+        let data = self.vars[a].data * self.vars[b].data;
+        self.push_var(data, vec![a, b], Op::Mul)
     }
 
-    fn relu(&mut self, a: VariableIdx) -> VariableIdx {
-        let va = &self.vars[a];
-        let new_var = Variable {
-            data: if va.data > 0.0 { va.data } else { 0.0 },
-            grad: None,
-            children: vec![a],
-            op: Op::ReLU,
-        };
-
-        self.vars.push(new_var);
-        self.vars.len() - 1
+    pub fn pow(&mut self, a: VariableIdx, exp: f64) -> VariableIdx {
+        let data = self.vars[a].data.powf(exp);
+        self.push_var(data, vec![a], Op::Pow(exp))
     }
 
-    fn neg(&mut self, a: VariableIdx) -> VariableIdx {
+    pub fn relu(&mut self, a: VariableIdx) -> VariableIdx {
+        let adata = self.vars[a].data;
+        let data = if adata > 0.0 { adata } else { 0.0 };
+        self.push_var(data, vec![a], Op::ReLU)
+    }
+
+    pub fn neg(&mut self, a: VariableIdx) -> VariableIdx {
         let minus_one = self.var(-1.0);
         self.mul(a, minus_one)
     }
 
-    fn sub(&mut self, a: VariableIdx, b: VariableIdx) -> VariableIdx {
+    pub fn sub(&mut self, a: VariableIdx, b: VariableIdx) -> VariableIdx {
         let neg_b = self.neg(b);
         self.add(a, neg_b)
     }
 
-    fn div(&mut self, a: VariableIdx, b: VariableIdx) -> VariableIdx {
+    pub fn div(&mut self, a: VariableIdx, b: VariableIdx) -> VariableIdx {
         let b_inv = self.pow(b, -1.0);
         self.mul(a, b_inv)
     }
@@ -125,7 +103,7 @@ impl Context {
         }
     }
 
-    fn backprop(&mut self, a: VariableIdx) {
+    pub fn backprop(&mut self, a: VariableIdx) {
         let mut topo = Vec::new();
         let mut visited = HashSet::new();
 
@@ -184,27 +162,16 @@ impl Variable {
 }
 
 fn main() {
+    // Test: b = a*a + a, where a=3
+    // b = 9 + 3 = 12
+    // db/da = 2a + 1 = 7
     let mut ctx = Context::new();
-    let a = ctx.var(1.0);
-    let b = ctx.var(2.);
-    let c = ctx.var(3.);
-    let d = ctx.add(a, b);
-    let e = ctx.mul(d, c);
-    let f = ctx.div(e, b);
-    let g = ctx.sub(f, c);
-    let h = ctx.relu(g);
+    let a = ctx.var(3.0);
+    let a_sq = ctx.mul(a, a);  // a*a = 9
+    let b = ctx.add(a_sq, a);  // a*a + a = 12
 
-    ctx.backprop(h);
+    ctx.backprop(b);
 
-    println!("{:?}", ctx.vars);
-    dbg!(
-        &ctx.vars[a],
-        &ctx.vars[b],
-        &ctx.vars[c],
-        &ctx.vars[d],
-        &ctx.vars[e],
-        &ctx.vars[f],
-        &ctx.vars[g],
-        &ctx.vars[h],
-    );
+    dbg!(&ctx.vars[a], &ctx.vars[a_sq], &ctx.vars[b]);
+    // Expected: a.grad = 7 (2*3 + 1)
 }
