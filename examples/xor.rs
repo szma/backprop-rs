7f0e8e401e622a9d9d33a1845aeffebf63120000
Markus Schütz <schuetz@mailbox.org>
2025-11-27 21:55:00 +0100

    move examples to examples

---
+++ examples/xor.rs
@@ -0,0 +1,52 @@
+#![allow(dead_code)]
+
+fn main() {
+    xor();
+}
+
+fn xor() {
+    use backprop_rs::graph::Graph;
+    let xs: Vec<[f64; 2]> = vec![[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]];
+    let ys: Vec<f64> = vec![0.0, 1.0, 1.0, 0.0];
+
+    let g = Graph::new();
+    let mlp = g.mlp(2, vec![32, 32, 1]);
+    let params = mlp.parameters();
+    let checkpoint = g.len();
+
+    let lr = 0.01;
+
+    for epoch in 0..500 {
+        let mut total_loss = g.variable(0.0);
+
+        for (x, &target) in xs.iter().zip(ys.iter()) {
+            let inputs = [g.variable(x[0]), g.variable(x[1])];
+            let pred = mlp.forward(&inputs);
+            let y_target = g.variable(target);
+
+            let loss = (pred[0]- y_target).pow(2.);
+            total_loss = total_loss + loss;
+        }
+
+        total_loss.backprop();
+
+        for &p in &params {
+            let grad = p.grad().unwrap_or(0.0);
+            p.set_data(p.data() - lr * grad);
+        }
+
+        if epoch % 100 == 0 {
+            println!("Epoch {}: Loss = {:.4}", epoch, total_loss.data());
+        }
+
+        g.zero_grad();
+        g.truncate(checkpoint);
+    }
+
+    println!("\nResults:");
+    for (x, &target) in xs.iter().zip(ys.iter()) {
+        let inputs = [g.variable(x[0]), g.variable(x[1])];
+        let pred = mlp.forward(&inputs);
+        println!("  {:?} -> {:.3} (expected: {})", x, pred[0].data(), target);
+    }
+}
